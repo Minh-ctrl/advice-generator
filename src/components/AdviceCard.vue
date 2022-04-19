2e32d6a541a3ca430b2d5befaf67f8d83f52e51f
minh-ctrl <binhminh140899@gmail.com>
2022-04-19 13:28:13 +0900

    feat: 🎸 working

---
+++ src/components/AdviceCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, reactive } from "vue";
+const data = ref(null);
+async function fetchData() {
+  const req = await fetch("https://api.adviceslip.com/advice");
+  const res = await req.json();
+  data.value = res;
+}
+</script>
+<template>
+  <div class="container">
+    <button @click="fetchData">clickme for data</button>
+    <div v-if="data">
+      {{ data }}
+    </div>
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AdviceCard from "../components/AdviceCard.vue";
+</script>
+<template>
+  <div class="about">
+    <AdviceCard />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
