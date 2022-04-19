e1bcca5007496253268c8ec12f46cc2e1f9a2de1
minh-ctrl <binhminh140899@gmail.com>
2022-04-19 22:01:23 +0900

    feat: 🎸 80% done

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,31 +3,25 @@ import {ref} from 'vue';
 const props = defineProps(["data"]);
 import IconDice from "../assets/IconDice.vue";
 import PatternDividerDesktop from "../assets/PatternDividerDesktop.vue";
-const click = ref(null);
-async function fetchData() {
-  const req = await fetch("https://api.adviceslip.com/advice");
-  const res = await req.json();
-  click.value = res;
-  console.log(click.value)
-}
+
 </script>
 <template>
-  <div
-    class="containerCard bg-zinc-700 flex flex-col flex-nowrap rounded-md"
-  >
+  <div class="containerCard flex flex-col flex-nowrap rounded-lg">
     <h3 class="id text-xs tracking-widest text-center self-center pt-5">
       ADVICE #{{ data.id }}
     </h3>
     <div class="advice self-center flex flex-row flex-nowrap justify-center">
-      <h1 class="text-center text-3xl justify-self-center self-center pt-5">"{{ data.advice }}"
+      <h1 class="text-center text-2xl justify-self-center self-center pt-5">"{{ data.advice }}"
       </h1>
     </div>
     <div class="svgContainer flex flex-col flex-nowrap">
-      <PatternDividerDesktop class="mb-10 self-center" />
-      <div
-        class="diceContainer flex flex-row justify-center flex-nowrap self-center bg-green-400 shadow-green-400 rounded-full"
-      >
-        <IconDice @click="fetchData" class="self-center" />
+      <PatternDividerDesktop class="pattern block self-center mb-10" />
+      <div class="containerofDice block">
+        <div
+          class="diceContainer flex flex-row justify-center flex-nowrap self-center bg-green-400 shadow-green-400 rounded-full"
+        >
+          <IconDice class="self-center" />
+        </div>
       </div>
     </div>
   </div>
@@ -40,14 +34,19 @@ async function fetchData() {
 .containerCard {
   height: 300px;
   width: 550px;
-  color: hsl(218, 23%, 16%);
+  background-color: hsl(218, 23%, 16%);
 }
 .advice {
   color: #cee3e8;
-  height: 400px;
+  height: 450px;
   width: 500px;
 }
-.svgContainer {
+.containerofDice {
+  left: 45%;
+  top: 25%;
+}
+.pattern{
+  top: 20%;
 }
 .diceContainer {
   width: 50px;
